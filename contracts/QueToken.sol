d77dedad97e6e3c808e23506206c3295def96d48
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-05 19:29:13 +0300

    v2.0.0

---
+++ contracts/QueToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: QUE_TOKEN
+pragma solidity >=0.4.17 <0.8.0;
+
+contract QueToken{
+     // Constructor
+     // Set the total number of tokens
+     // Read the total number of tokens
+     uint256 public totalSupply;
+     function QueCoin() public{
+          totalSupply=1000000;
+     }
+}
